a91636be32850dfe430fd7acdbb4a4e4b305924b
WINDOWS10\kazuy <zakrain@b.email.ne.jp>
2018-02-18 11:04:32 +0900

    add SVM

--- titanic.R
+++ titanic.R
@@ -24,31 +24,20 @@ test <- read.csv("test.csv", stringsAsFactors=F, na.strings = c("NA",""))
 # 結合
 test$Survived <- NA
 d <- rbind(train, test)
-
 str(d)
-summary(d)
-head(d)
 
-# 欠損値 Cabin
-apply(is.na(d), 2, sum)
-sapply(d, function(x) {sum(is.na(x))})
+# ----- 前処理 -----
 
-# 称号(title)
-d$Title <- sapply(d$Name, function(x) {strsplit(x, split='[,.]')[[1]][2]})
-d$Title <- trimws(d$Title, "both")
-
-# クロス集計
-table(d$Sex)
-table(d$Pclass)
-table(d$Embarked)
+# 称号(Title)
+d$Title <- sapply(d$Name, function(x) {trimws(strsplit(x, split='[,.]')[[1]][2], "both")})
 table(d$Title)
 
-d$Sex <- as.factor(d$Sex)
-d$Pclass <- as.factor(d$Pclass)
-d$Embarked <- as.factor(d$Embarked)
-d$Title <- as.factor(d$Title)
+d[d$Title=="Don",]
+
 
 # 欠損値を補間
+sapply(d, function(x) {sum(is.na(x))})
+
 a <- tapply(d$Age, d$Title, function(x) {mean(x, na.rm=T)})
 b <- d[is.na(d$Survived) & is.na(d$Age), "Title"]
 table(b)
@@ -58,12 +47,36 @@ d[is.na(d$Survived) & is.na(d$Age) & d$Title=="Mr", "Age"] <- a["Mr"]
 d[is.na(d$Survived) & is.na(d$Age) & d$Title=="Mrs", "Age"] <- a["Mrs"]
 d[is.na(d$Survived) & is.na(d$Age) & d$Title=="Ms", "Age"] <- a["Ms"]
 
-d[is.na(d$Fare),]
-d[is.na(d$Fare), "Fare"] <- median(d[d$Pclass==3, "Fare"], na.rm=T)
+d[is.na(d$Survived) & is.na(d$Fare),]
+d[is.na(d$Survived) & is.na(d$Fare), "Fare"] <- median(d[d$Pclass==3, "Fare"], na.rm=T)
+d[is.na(d$Survived) & d$Fare==0,]
+d[is.na(d$Survived) & d$Fare==0, "Fare"] <- median(d[d$Pclass==1, "Fare"], na.rm=T)
 
-apply(is.na(d[is.na(d$Survived),]), 2, sum)
+# ファクター型変換
+lapply(d, function(x) length(unique(x)))
+d$Sex <- as.factor(d$Sex)
+d$Pclass <- as.factor(d$Pclass)
+d$Embarked <- as.factor(d$Embarked)
+d$Title <- as.factor(d$Title)
 
-# TODO - Fare == 0
+# データ分離
+train <- d[!is.na(d$Survived),]
+test <- d[is.na(d$Survived),]
+
+train$PassengerId <- NULL
+train$Name <- NULL
+train$Ticket <- NULL
+train$Cabin <- NULL
+
+train <- na.omit(train)
+train <- train[!train$Fare==0,]
+str(train)
+
+# クロス集計
+table(d$Sex)
+table(d$Pclass)
+table(d$Embarked)
+table(d$Title)
 
 # 生存率
 table(d$Survived)
@@ -81,7 +94,6 @@ train$Survived <- as.factor(train$Survived)
 plot(train$Pclass,train$Survived)
 plot(train$Sex, train$Survived)
 
-
 # TODO - 相関
 cor(d)
 hist(d$Age)
@@ -91,16 +103,10 @@ plot(d$Sex)
 pairs(d, panel=panel.smooth)
 pairs.panels(d)
 
+
 # ----- Decision Tree -----
 library(rpart)
 library(rpart.plot)
-
-train <- d[!is.na(d$Survived),]
-test <- d[is.na(d$Survived),]
-
-train$Name <- NULL
-train$Ticket <- NULL
-train$Cabin <- NULL
 
 m <- rpart(Survived~., train)
 rpart.plot(m)
@@ -121,18 +127,38 @@ train$Ticket <- NULL
 train$Cabin <- NULL
 
 train <- na.omit(train)
-m <- randomForest(as.factor(Survived)~., d=train)
+train <- train[!train$Fare==0,]
+test[test$Fare==0,"Fare"] <- median(d[d$Pclass==1, "Fare"], na.rm=T)
+
+set.seed(1234)
+m <- randomForest(as.factor(Survived)~., d=train, mtry=2, ntree=500)
 m
 varImpPlot(m)
+plot(m)
 
 pred <- predict(m, test)
 pred
 a <- data.frame(PassengerId=test$PassengerId, Survived=pred)
 write.csv(a, "answer.csv", quote=FALSE, row.names=FALSE)
 
-# TODO - tuneRF
-
+# tuneRF
+tuneRF(train[,-1], as.factor(train[,1]), doBest=T, ntreeTry=500)
 
 # ----- svM -----
+library(e1071)
+library(kernlab)
 
+train <- d[!is.na(d$Survived),]
+test <- d[is.na(d$Survived),]
+
+train$PassengerId <- NULL
+train$Name <- NULL
+train$Ticket <- NULL
+train$Cabin <- NULL
+
+m <- ksvm(as.factor(Survived)~., d=train)
+pred <- predict(m, test)
+pred
+a <- data.frame(PassengerId=test$PassengerId, Survived=pred)
+write.csv(a, "answer.csv", quote=FALSE, row.names=FALSE)
 
