43ed02882bd88388f9fea2206395b05a204d5ac9
WINDOWS10\kazuy <zakrain@b.email.ne.jp>
2018-02-22 21:11:31 +0900

    developing

--- titanic.R
+++ titanic.R
@@ -17,6 +17,8 @@
 
 library(psych)
 
+# ----- データ読込み -----
+
 setwd("d:/kaggle/titanic")
 train <- read.csv("train.csv", stringsAsFactors=F, na.strings = c("NA",""))
 test <- read.csv("test.csv", stringsAsFactors=F, na.strings = c("NA",""))
@@ -30,27 +32,48 @@ str(d)
 
 # 称号(Title)
 d$Title <- sapply(d$Name, function(x) {trimws(strsplit(x, split='[,.]')[[1]][2], "both")})
+d[d$Title=="Mlle", "Title"] <- "Miss"
+d[d$Title=="Mme", "Title"] <- "Mrs"
 table(d$Title)
 
-d[d$Title=="Don",]
+# 欠損値の補完(Fare)
+a <- aggregate(Fare~Pclass, train, median)
+d[(is.na(d$Fare) | d$Fare==0) & d$Pclass==1, "Fare"] <- a[a$Pclass==1, "Fare"]
+d[(is.na(d$Fare) | d$Fare==0) & d$Pclass==2, "Fare"] <- a[a$Pclass==2, "Fare"]
+d[(is.na(d$Fare) | d$Fare==0) & d$Pclass==3, "Fare"] <- a[a$Pclass==3, "Fare"]
 
+# 欠損値の補完(Fare)
+d[is.na(d$Embarked), "Embarked"] <- "S"
 
-# 欠損値を補間
+# 欠損値を補間(Age)
+m <- glm(Age ~ Title + Fare + SibSp + Sex -1, data = d)
+
+m <- glm(Age~Title+Fare+SibSp+Parch+Sex, data = d[!is.na(d$Age),])
+m <- randomForest(Age~Title+Fare+SibSp+Sex, data = d[!is.na(d$Age),], action=na.ommit)
+
+pred <- predict(m, d[is.na(d$Age),])
+pred
+d[is.na(d$Age),"Age"] <- pred
+d[d$Age < 0, "Age"] <- 0
+
 sapply(d, function(x) {sum(is.na(x))})
 
+# 欠損値を補間(Age)
 a <- tapply(d$Age, d$Title, function(x) {mean(x, na.rm=T)})
 b <- d[is.na(d$Survived) & is.na(d$Age), "Title"]
 table(b)
-d[is.na(d$Survived) & is.na(d$Age) & d$Title=="Master", "Age"] <- a["Master"]
-d[is.na(d$Survived) & is.na(d$Age) & d$Title=="Miss", "Age"] <- a["Miss"]
-d[is.na(d$Survived) & is.na(d$Age) & d$Title=="Mr", "Age"] <- a["Mr"]
-d[is.na(d$Survived) & is.na(d$Age) & d$Title=="Mrs", "Age"] <- a["Mrs"]
-d[is.na(d$Survived) & is.na(d$Age) & d$Title=="Ms", "Age"] <- a["Ms"]
+#d[is.na(d$Survived) & is.na(d$Age) & d$Title=="Master", "Age"] <- a["Master"]
+#d[is.na(d$Survived) & is.na(d$Age) & d$Title=="Miss", "Age"] <- a["Miss"]
+#d[is.na(d$Survived) & is.na(d$Age) & d$Title=="Mr", "Age"] <- a["Mr"]
+#d[is.na(d$Survived) & is.na(d$Age) & d$Title=="Mrs", "Age"] <- a["Mrs"]
+#d[is.na(d$Survived) & is.na(d$Age) & d$Title=="Ms", "Age"] <- a["Ms"]
 
-d[is.na(d$Survived) & is.na(d$Fare),]
-d[is.na(d$Survived) & is.na(d$Fare), "Fare"] <- median(d[d$Pclass==3, "Fare"], na.rm=T)
-d[is.na(d$Survived) & d$Fare==0,]
-d[is.na(d$Survived) & d$Fare==0, "Fare"] <- median(d[d$Pclass==1, "Fare"], na.rm=T)
+d[is.na(d$Age) & d$Title=="Master", "Age"] <- a["Master"]
+d[is.na(d$Age) & d$Title=="Miss", "Age"] <- a["Miss"]
+d[is.na(d$Age) & d$Title=="Mr", "Age"] <- a["Mr"]
+d[is.na(d$Age) & d$Title=="Mrs", "Age"] <- a["Mrs"]
+d[is.na(d$Age) & d$Title=="Ms", "Age"] <- a["Ms"]
+d[is.na(d$Age) & d$Title=="Dr", "Age"] <- a["Dr"]
 
 # ファクター型変換
 lapply(d, function(x) length(unique(x)))
@@ -60,39 +83,46 @@ d$Embarked <- as.factor(d$Embarked)
 d$Title <- as.factor(d$Title)
 
 # データ分離
+d$Name <- NULL
+d$Ticket <- NULL
+d$Cabin <- NULL
+d$Embarked <- NULL
+
 train <- d[!is.na(d$Survived),]
 test <- d[is.na(d$Survived),]
 
 train$PassengerId <- NULL
-train$Name <- NULL
-train$Ticket <- NULL
-train$Cabin <- NULL
 
-train <- na.omit(train)
-train <- train[!train$Fare==0,]
+#train <- na.omit(train)
+#train <- train[!train$Fare==0,]
 str(train)
 
-# クロス集計
-table(d$Sex)
-table(d$Pclass)
-table(d$Embarked)
-table(d$Title)
+# ----- データ可視化 -----
 
-# 生存率
-table(d$Survived)
+# all
+table(train$Survived)
+addmargins(table(train$Survived))
 addmargins(round(prop.table(table(d$Survived))*100,2))
+plot(as.factor(train$Survived))
+
+# Sex
 addmargins(table(d$Sex,d$Survived))
 addmargins(round(prop.table(table(d$Sex, d$Survived))*100,2))
+plot(as.factor(train$Sex), as.factor(train$Survived))
+
+# Pclass
 addmargins(table(d$Pclass,d$Survived))
 addmargins(round(prop.table(table(d$Pclass, d$Survived))*100,2))
+plot(train$Pclass, as.factor(train$Survived))
 
-# TODO - グラフ化
-plot(as.factor(train$Survived))
+# Age
+hist(d$Age)
+
+# Fare
+aggregate(Fare~Pclass,train,median)
 hist(d$Fare)
 
-train$Survived <- as.factor(train$Survived)
-plot(train$Pclass,train$Survived)
-plot(train$Sex, train$Survived)
+#train$Survived <- as.factor(train$Survived)
 
 # TODO - 相関
 cor(d)
@@ -118,20 +148,8 @@ write.csv(a, "answer.csv", quote=FALSE, row.names=FALSE)
 # ----- Random Forest -----
 library(randomForest)
 
-train <- d[!is.na(d$Survived),]
-test <- d[is.na(d$Survived),]
-
-train$PassengerId <- NULL
-train$Name <- NULL
-train$Ticket <- NULL
-train$Cabin <- NULL
-
-train <- na.omit(train)
-train <- train[!train$Fare==0,]
-test[test$Fare==0,"Fare"] <- median(d[d$Pclass==1, "Fare"], na.rm=T)
-
 set.seed(1234)
-m <- randomForest(as.factor(Survived)~., d=train, mtry=2, ntree=500)
+m <- randomForest(as.factor(Survived)~., d=train, mtry=4, ntree=500)
 m
 varImpPlot(m)
 plot(m)
@@ -147,14 +165,6 @@ tuneRF(train[,-1], as.factor(train[,1]), doBest=T, ntreeTry=500)
 # ----- svM -----
 library(e1071)
 library(kernlab)
-
-train <- d[!is.na(d$Survived),]
-test <- d[is.na(d$Survived),]
-
-train$PassengerId <- NULL
-train$Name <- NULL
-train$Ticket <- NULL
-train$Cabin <- NULL
 
 m <- ksvm(as.factor(Survived)~., d=train)
 pred <- predict(m, test)
